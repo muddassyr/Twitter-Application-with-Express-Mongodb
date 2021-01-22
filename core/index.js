7c7d1fd8c3419bf36eddf43b08b3de3e1e964ea0
Muddassir Khan <61146053+muddassirk@users.noreply.github.com>
2021-01-23 00:36:32 +0500

    remove link

--- core/index.js
+++ core/index.js
@@ -1,7 +1,7 @@
 // var DBURI =
 
 module.exports = {
-    DBURI: process.env.DBURI || "mongodb+srv://legend:legend123@cluster0.2c3x6.mongodb.net/testdb?retryWrites=true&w=majority",
+    DBURI: process.env.DBURI,
     SERVER_SECRET: process.env.SECRET || "1234",
     CLIENT_SEC: process.env.CLIENT_SEC || "abc"
 
